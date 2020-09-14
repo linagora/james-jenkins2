312af0f6205f8c3a88287e3b2a530dafe3800fbb
Rene Cordier <rcordier@linagora.com>
2020-09-14 14:06:22 +0700

    JAMES-3028 Drop Swift configuration in profile

--- stress-job.groovy
+++ stress-job.groovy
@@ -5,7 +5,7 @@ pipelineJob("Stress-job") {
         stringParam('BRANCH', '', 'James branch')
         stringParam('SIMULATION', '', 'Gatling simulation')
         stringParam('GITHUB_SIMULATIONS', '', 'Github repository containing gatling simulations')
-        stringParam('PROFILE', 'reference', 'Blobs store target: reference (default, for over-time performance tracking), swift or s3')
+        stringParam('PROFILE', 'reference', 'Blobs store target: reference (default, for over-time performance tracking), s3 or s3-local')
     }
 
     definition {
